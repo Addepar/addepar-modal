90843af63ba89f4ff18fa2bba9533c4c5178889c
Billy Littlefield <billy.littlefield@addepar.com>
2018-05-22 10:58:35 -0700

    add ember-decorators/argument

--- addon/components/adde-modal.js
+++ addon/components/adde-modal.js
@@ -1,5 +1,8 @@
 import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
 
+import { argument } from '@ember-decorators/argument';
+
 export default class AddeModal extends ModalDialog {
+  @argument
   targetAttachment = 'center';
 }
