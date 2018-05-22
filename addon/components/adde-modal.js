11f0b31b5824d23171c1ece0c3f2eb3ac7000f1f
Billy Littlefield <billy.littlefield@addepar.com>
2018-05-22 10:23:55 -0700

    class syntax, extend modal-dialog

--- addon/components/adde-modal.js
+++ addon/components/adde-modal.js
@@ -1,6 +1,8 @@
-import Component from '@ember/component';
+import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
 import layout from '../templates/components/adde-modal';
 
-export default Component.extend({
-  layout
-});
+export default class AddeModal extends ModalDialog {
+  layout = layout;
+
+  targetAttachment = 'center';
+}
