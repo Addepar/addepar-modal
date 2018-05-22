f67697b104efbc842d7eaf423941ec709adf9cdf
Billy Littlefield <billy.littlefield@addepar.com>
2018-05-22 10:54:09 -0700

    add styling

--- addon/components/adde-modal.js
+++ addon/components/adde-modal.js
@@ -1,8 +1,5 @@
 import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
-import layout from '../templates/components/adde-modal';
 
 export default class AddeModal extends ModalDialog {
-  layout = layout;
-
   targetAttachment = 'center';
 }
