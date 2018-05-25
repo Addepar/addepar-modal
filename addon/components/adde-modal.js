0c12f3d1a425a8823b55bd5463c808bc7f923ded
Billy Littlefield <billy.littlefield@addepar.com>
2018-05-25 14:05:32 -0700

    styling for header and footer

--- addon/components/adde-modal.js
+++ addon/components/adde-modal.js
@@ -1,8 +1,37 @@
 import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
 
+import layout from '../templates/components/adde-modal';
+
 import { argument } from '@ember-decorators/argument';
+import { computed } from '@ember-decorators/object';
 
 export default class AddeModal extends ModalDialog {
+  layout = layout;
+
   @argument
   targetAttachment = 'center';
+
+  @argument
+  confirmText = 'Confirm';
+
+  @argument
+  closeText = 'Cancel';
+
+  @argument
+  headerText = '';
+
+  @argument
+  size = 'normal';
+
+  @computed('size')
+  get sizeClass() {
+    let size = this.get('size');
+    if (size === 'large') {
+      return 'size-lg';
+    } else if (size === 'small') {
+      return 'size-sm';
+    } else {
+      return '';
+    }
+  }
 }
