c03471bb829418014bbf5851b9a1e2d4363512c9
Billy Littlefield <billy.littlefield@addepar.com>
2018-05-30 11:02:14 -0700

    add babel-eslint for es6 experimental features, turn off ember eslint rules for closure actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ['@addepar', '@addepar/eslint-config/ember']
+  extends: ['@addepar', '@addepar/eslint-config/ember'],
+  parser: 'babel-eslint',
+  rules: {
+    // 'emer-best-practices/require-dependent-keys' gives false positives with ember decorator
+    // syntax. this is a known issue: https://github.com/ember-best-practices/eslint-plugin-ember-best-practices/issues/97
+    'ember-best-practices/require-dependent-keys': false,
+    // Until we drop support for < ember 2
+    'ember/closure-actions': false
+  }
 };
